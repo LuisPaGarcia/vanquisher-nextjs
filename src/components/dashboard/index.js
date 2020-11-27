6b127353f86f55b80ebea9b4ee0232cf695f7eb7
LuisPa <diganluispa@gmail.com>
2020-11-26 18:38:58 -0600

    feat: Testing <Image /> next.js component

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import NavTest from "components/NavTest";
+import Image from "next/image";
 
 function Dashboard(props) {
   return (
@@ -8,6 +9,12 @@ function Dashboard(props) {
       <h1 className="text-3xl text-center pt-4 text-white bg-gray-800">
         Dashboard
       </h1>
+      <Image
+        src="/static/dog.jpeg"
+        width="400"
+        height="400"
+        alt="dog picture"
+      />
     </>
   );
 }
